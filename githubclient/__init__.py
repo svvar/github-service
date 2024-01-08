550a96b20b23138dc34a4f00238a814bd49be734
svvar <aleksseva003@gmail.com>
2024-01-08 11:59:14 +0200

    Added custom exceptions, checked with linter

---
+++ githubclient/__init__.py
@@ -0,0 +1,9 @@
+"""
+Simple python module that provides a client for the GitHub API.
+
+Supports the following operations:
+- Fetching user information
+- Fetching user repositories
+- Creating new repositories
+- Deleting existing repositories
+"""
--- githubclient/github_client.py
+++ githubclient/github_client.py
@@ -1,74 +1,78 @@
-
-import requests
+"""GitHub API client implementation."""
 import configparser
 
+import requests
 
-class GithubClient:
+from githubclient.exceptions import (
+    InvalidAccessTokenError,
+    RepositoryCreationError,
+    RepositoryDeletionError,
+    UserNotFoundError,
+)
+
+
+class GithubClient(object):
     """
     Class to make some actions using GitHub API.
+
     API key must be added to key.ini file
     """
 
     def __new__(cls, *args, **kwargs):
-        """
-        Basic singleton implementation for githubclient class
-
-        """
-        if not hasattr(cls, '_instance'):
+        """Implement simple singleton for GithubClient class."""
+        if not getattr(cls, '_instance', None):
             cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
+        """Initialize GithubClient class."""
         config = configparser.ConfigParser()
-        config.read('key.ini')
+        config.read('githubclient/key.ini')
 
-        self.__api_key = config['API']['GITHUB_KEY']        # GitHub API key from key.ini
-        self.url = 'https://api.github.com'                 # GitHub API URL
-        self.__user = self.__get_username_for_key()         # GitHub API key owner's username
-                                                            # (Task: storing some retrieved data locally)
-
-    def __get_username_for_key(self):
-        """
-        Used to fetch the username of the GitHub API key owner
-
-        """
-        response = requests.get(f'{self.url}/user', headers={'Authorization': f'Bearer {self.__api_key}'})
-
-        if response.status_code != requests.status_codes.codes.OK:
-            raise Exception('Invalid API key')
-        return response.json()['login']
+        self._api_key = config['API']['GITHUB_KEY']
+        self.url = 'https://api.github.com'
+        self._user = self._get_username_for_key()  # (Task: storing some retrieved data locally)
 
     def get_user_info(self, username: str):
         """
-        Fetches user information for a given username
+        Fetch user information for a given username.
 
         :param username:
         :return: dict with all information about the user
+
+        :raises: UserNotFoundError if user is not found
         """
-        response = requests.get(f'{self.url}/users/{username}')
+        response = requests.get(
+            '{0}/users/{1}'.format(self.url, username),
+            timeout=1,
+        )
 
         if response.status_code != requests.status_codes.codes.OK:
-            raise Exception(response.json()['message'])
+            raise UserNotFoundError(username)
         return response.json()
 
     def get_user_repos(self, username: str):
         """
-        Fetches all repositories of a user
+        Fetch all repositories of a user.
 
         :param username: username whose repositories are to be fetched
         :return: list of repository names
+
+        :raises: UserNotFoundError if user is not found
         """
-        response = requests.get(f'{self.url}/users/{username}/repos')
+        response = requests.get(
+            '{0}/users/{1}/repos'.format(self.url, username),
+            timeout=1,
+        )
 
         if response.status_code != requests.status_codes.codes.OK:
-            raise Exception(response.json()['message'])
+            raise UserNotFoundError(username)
 
-        repo_names = [r['name'] for r in response.json()]
-        return repo_names
+        return [repo['name'] for repo in response.json()]
 
     def create_new_repo(self, repo_name: str, description: str = None, private: bool = False):
         """
-        Creates a new repository for the owner of the GitHub API key in key.ini
+        Create a new repository for the owner of the GitHub API key in key.ini .
 
         :param repo_name: name of the repository to be created
         :param description: description of the repository
@@ -76,35 +80,53 @@ class GithubClient:
 
         :return: url of the created repository
 
-        :raises: Exception if repository creation fails
+        :raises: RepositoryCreationError if repository creation fails
         """
-
-        response = requests.post(f'{self.url}/user/repos', json={'name': repo_name,
-                                                                 'description': description,
-                                                                 'private': private},
-                                 headers={'Authorization': f'Bearer {self.__api_key}'})
+        response = requests.post(
+            '{0}/user/repos'.format(self.url),
+            json={'name': repo_name, 'description': description, 'private': private},
+            headers={'Authorization': 'Bearer {0}'.format(self._api_key)},
+            timeout=1,
+        )
 
         if response.status_code != requests.status_codes.codes.CREATED:
-            error_details = ''
-            if 'errors' in response.json:
-                error_details = response.json()['errors'][0]['message']
-            raise Exception(f"{response.json()['message']} {error_details}")
+            raise RepositoryCreationError(response.json())
+
         return response.json()['html_url']
 
     def delete_repo(self, repo_name: str):
         """
-        Deletes a repository from GitHub API key owner's account
+        Delete a repository from GitHub API key owner's account.
 
         :param repo_name: repository to be deleted
         :return: True if deletion is successful
 
-        :raises: Exception if repository deletion fails
+        :raises: RepositoryDeletionError if repository deletion fails
         """
-
-        response = requests.delete(f"{self.url}/repos/{self.__user}/{repo_name}",
-                                   headers={'Authorization': f'Bearer {self.__api_key}'})
+        response = requests.delete(
+            '{0}/repos/{1}/{2}'.format(self.url, self._user, repo_name),
+            headers={'Authorization': 'Bearer {0}'.format(self._api_key)},
+            timeout=1,
+        )
 
         if response.status_code != requests.status_codes.codes.NO_CONTENT:
-            raise Exception(f'Repository {repo_name} deletion failed. {response.json()["message"]}')
+            raise RepositoryDeletionError(response.json())
         return True
 
+    def _get_username_for_key(self):
+        """
+        Use to fetch the username of the GitHub API key owner.
+
+        :return: username of the GitHub API key owner
+
+        :raises: InvalidAccessTokenError if the access token in key.ini is not associated with any real profile
+        """
+        response = requests.get(
+            '{0}/user'.format(self.url),
+            headers={'Authorization': 'Bearer {0}'.format(self._api_key)},
+            timeout=1,
+        )
+
+        if response.status_code != requests.status_codes.codes.OK:
+            raise InvalidAccessTokenError()
+        return response.json()['login']
